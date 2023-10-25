f0861532301af4812544073b62990f6790476fc6
朵朵 <411221259@gms.ndhu.edu.tw>
2023-10-25 11:21:38 +0800

    Update assert.c

--- assert.c
+++ assert.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
 
-assert(expression);
-void _assert(
-   char const* message,
-   char const* filename,
-   unsigned line
-);
-void _wassert(
-   wchar_t const* message,
-   wchar_t const* filename,
-   unsigned line
-);
-
 int main(){    
     
     return 0;
