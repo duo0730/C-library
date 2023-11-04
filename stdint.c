d823d400f20935a74f36f09fa6925f53ab6799b3
朵朵 <411221259@gms.ndhu.edu.tw>
2023-11-04 20:53:16 +0800

    Update stdint.c

--- stdint.c
+++ stdint.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/*
+int8_t：有符號8位整數。
+uint8_t：無符號8位整數。
+int16_t：有符號16位整數。
+uint16_t：無符號16位整數。
+int32_t：有符號32位整數。
+uint32_t：無符號32位整數。
+int64_t：有符號64位整數。
+uint64_t：無符號64位整數。
+int_least8_t：至少8位的有符號整數。
+uint_least8_t：至少8位的無符號整數。
+int_fast8_t：適合快速計算的有符號8位整數。
+uint_fast8_t：適合快速計算的無符號8位整數。
+*/
+
 int main(){
     
     return 0;
