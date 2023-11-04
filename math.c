a0f59cbff5acfd53f38368c31cf9574821d839f3
朵朵 <411221259@gms.ndhu.edu.tw>
2023-11-04 20:47:48 +0800

    Update math.c

--- math.c
+++ math.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+double sqrt(double x)：計算x的平方根
+double pow(double base, double exponent)：計算 base 的 exponent 次幂。
+double sin(double x)：計算正弦函數值，x為弧度
+double cos(double x)：計算餘弦函數值，x為弧度
+double tan(double x)：計算正切弦函數值，x為弧度
+double log(double x)：計算以e為底的自然對數
+double log10(double x)：幾算以10為底的自然對數
+double fabs(double x)：返回x的絕對值。
+double ceil(double x)：向上取整，返回不小於 x的最小整数。
+double floor(double x)：向下取整，返回不大於 x的最大整数。
+double round(double x)：四舍五入，返回最接近 x的整数。
+*/
+
 int main(){
     
     return 0;
