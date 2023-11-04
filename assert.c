48c648d0d2753b8c74bd58e08ed3384201c207ad
朵朵 <411221259@gms.ndhu.edu.tw>
2023-11-04 20:47:41 +0800

    Update assert.c

--- assert.c
+++ assert.c
@@ -3,5 +3,13 @@
 
 int main(){    
     
+    int a=10;
+    assert(a==10);
+    //如果a不等於10則程式終止並跳出錯誤訊息
+
+    int x = 10;
+    assert(x == 5 && "x should be equal to 5");
+    //自定義錯誤訊息
+
     return 0;
 }
