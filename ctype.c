1228b41e0767908a17eb0f4833f870c43cd53c49
朵朵 <411221259@gms.ndhu.edu.tw>
2023-11-04 20:47:45 +0800

    Update ctype.c

--- ctype.c
+++ ctype.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/*
+isalpha(int c)：檢查字符c是否為一個英文字母
+isdigit(int c)：檢查字符c是否為一個數字
+isalnum(int c)：檢查字符c是否為字母或數字
+islower(int c)：檢查字符c是否為小寫字母
+isupper(int c)：檢查字符c是否為大寫字母
+isspace(int c)：檢查字符c是否為為空白字符（空格、換行等）
+tolower(int c)：將c轉換為小寫字母
+toupper(int c)：將c轉換為大寫字母
+*/
+
 int main(){
 
     return 0;
-}
+}   
