419a283b9a01d99117905283806181133919df33
朵朵 <411221259@gms.ndhu.edu.tw>
2023-12-20 11:24:16 +0800

    Update assert.c

--- assert.c
+++ assert.c
@@ -7,9 +7,21 @@ int main(){
     assert(a==10);
     //如果a不等於10則程式終止並跳出錯誤訊息
 
-    int x = 10;
-    assert(x == 5 && "x should be equal to 5");
+    int b=10;
+    assert(b==5 && "b should be equal to 5");
     //自定義錯誤訊息
+
+    int c;
+    char str[50];
+
+    scanf("%d", &a);
+    assert(c>=10);
+    printf("%d\n", a);
+
+    scanf("%s", str);
+    assert(str!=NULL);
+    printf("%s\n", str);
+
 
     return 0;
 }
